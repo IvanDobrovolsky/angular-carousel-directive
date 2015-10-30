30a413d5e4ca1710bf879614f6f462be151f66f8
IvanDobrovolsky <dobrovolsky94@gmail.com>
2015-10-30 19:42:23 +0200

    Created application module and apllication controller files. The controller was registered with the application module.

---
+++ app/app.controller.js
@@ -0,0 +1,30 @@
+(function () {
+    "use strict";
+
+    angular.module('carouselTestApp')
+        .controller("AppController", [AppController]);
+
+    function AppController(){
+        var vm = this;
+
+        vm.slides1 = [
+            "assets/images/1.jpg",
+            "assets/images/2.jpg",
+            "assets/images/3.jpg",
+            "assets/images/4.jpg",
+            "assets/images/5.jpg",
+            "assets/images/6.jpg"
+        ];
+
+        vm.slides2 = [
+            "assets/images/5.jpg",
+            "assets/images/6.jpg",
+            "assets/images/1.jpg",
+            "assets/images/2.jpg",
+            "assets/images/3.jpg",
+            "assets/images/4.jpg"
+        ];
+
+    }
+
+}());
