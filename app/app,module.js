43be521b755f3597850c4d4edf7e3a4b0cbd430b
IvanDobrovolsky <dobrovolsky94@gmail.com>
2015-10-30 20:16:18 +0200

    Added all needed dependecies to index.html file

--- app/app,module.js
+++
@@ -1,6 +0,0 @@
-(function () {
-    "use strict";
-
-    var carousel = angular.module('carouselTestApp', ["carousel"]);
-
-}());
---
+++ app/app.module.js
@@ -0,0 +1,6 @@
+(function () {
+    "use strict";
+
+    var carousel = angular.module('carouselApp', ["carousel"]);
+
+}());
